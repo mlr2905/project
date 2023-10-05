892463bc2a4495576443330541a740d02222c025
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2023-10-05 09:54:59 +0300

    ופ

---
+++ Memory-Game-&-API/Api.js
@@ -0,0 +1,28 @@
+
+class Api {
+    constructor(_data1, _data2, _data3, _data4) {
+
+      this.data1 = _data1
+      this.data2 = _data2
+      this.data3 = _data3
+      this.data4 = _data4
+
+
+    }
+    set_data1(_data1) {
+      this.data1 = _data1;
+    }
+    set_data2(_data2) {
+      this.data2 = _data2;
+    }
+    set_data3(_data3) {
+      this.data3 = _data3;
+    }
+    set_data3(_data4) {
+      this.data4 = _data4;
+    }
+
+  }
+
+
+
---
+++ Memory-Game-&-API/Board.js
@@ -0,0 +1,14 @@
+class Board {
+    constructor(_pairs, _maxarr, _doubled, _freeze, _arr, _Creating_a_div, _Two_numbers_use, _game_type, _style) {
+      this.pairs = _pairs;
+      this.maxarr = _maxarr;
+      this.doubled = _doubled;
+      this.freeze = _freeze
+      this.arr = _arr;
+      this.Creating_a_div = _Creating_a_div; // מערך זה משמש ליצרת כמות הקלפים שמופיעים בלוח
+      this.Two_numbers_use = _Two_numbers_use;//יצרת מערך שמשמש לבדיקת התאמה של הקלפים הפתוחים
+      this.game_type = _game_type
+      this.style = _style
+
+    }
+  }
