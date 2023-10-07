a75051770d60578bbe7125510b9d984083f52f8b
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2023-10-08 00:57:50 +0300

    up

---
+++ Memory-Game-&-API/Style_cards.js
@@ -0,0 +1,10 @@
+class Style_cards {
+    constructor(_card_back, _catd_front, _card_over,_imgN) {
+      this.card_back = _card_back;
+      this.catd_front = _catd_front;
+      this.card_over = _card_over;
+      this.imgN = _imgN
+
+
+    }
+  }
